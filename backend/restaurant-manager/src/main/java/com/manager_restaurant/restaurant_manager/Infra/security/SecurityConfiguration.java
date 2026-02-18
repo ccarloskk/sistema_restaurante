c7dbe6aca66280741b4dcb202317b270ead5c0bf
Carlos Henrique <carloshsc000@gmail.com>
2026-02-17 21:46:04 -0300

    Adicionado PUT no security

--- backend/restaurant-manager/src/main/java/com/manager_restaurant/restaurant_manager/Infra/security/SecurityConfiguration.java
+++ backend/restaurant-manager/src/main/java/com/manager_restaurant/restaurant_manager/Infra/security/SecurityConfiguration.java
@@ -54,6 +54,7 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.GET, "/orders/details").permitAll()
                         .requestMatchers(HttpMethod.POST, "/orders/updateTotalPublic").permitAll()
                         .requestMatchers(HttpMethod.POST, "/orders/createOrder").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/orders/{id_order}").permitAll()
                         .requestMatchers(HttpMethod.DELETE, "/orders/**").permitAll()
 
                         .requestMatchers(HttpMethod.GET, "/order_item/details").permitAll()
